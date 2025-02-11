98b167e39d69d0599a764a27feb8a9f927a5b13c
Mikael Lindberg <mikael.lindberg93@gmail.com>
2025-02-11 04:38:05 +0100

    added variables

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
-    echo 'Hello From PHP '
+    $title = 'Site PHP';
+    $heading = 'Welcome to the site';
+    $body = 'Some description string';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,23 +10,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title>PHP Primer</title>
+    <title><?= $title ?></title>
 </head>
 
 <body class="bg-gray-100">
     <header class="bg-blue-500 text-white p-4">
         <div class="container mx-auto">
             <h1 class="text-3xl font-semibold">
-                <?= 'Title'?>
+                <?= $title ?>
             </h1>
         </div>
     </header>
     <div class="container mx-auto p-4 mt-4">
         <div class="bg-white rounded-lg shadow-md p-6">
             <h2 class="text-2xl font-semibold mb-4">
-                <?= 'Welcome'?>
+                <?= $heading ?>
             </h2>
-                <?php echo '<p>Some description string</p>'; ?>
+            <p>
+                <?= $body ?>
+            </p>
         </div>
     </div>
 </body>
