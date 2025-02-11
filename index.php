b9d83947175e4b760e50fdb7361a8a01295c0eea
Mikael Lindberg <mikael.lindberg93@gmail.com>
2025-02-10 19:02:18 -0800

    tested inline php

--- index.php
+++ index.php
@@ -14,15 +14,18 @@
 <body class="bg-gray-100">
     <header class="bg-blue-500 text-white p-4">
         <div class="container mx-auto">
-            <h1 class="text-3xl font-semibold">WOOOOOO</h1>
+            <h1 class="text-3xl font-semibold">
+                <?= 'Title'?>
+            </h1>
         </div>
     </header>
     <div class="container mx-auto p-4 mt-4">
         <div class="bg-white rounded-lg shadow-md p-6">
-            <h2 class="text-2xl font-semibold mb-4">PHP Primer</h2>
-            <p>Let's get going bois.</p>
+            <h2 class="text-2xl font-semibold mb-4">
+                <?= 'Welcome'?>
+            </h2>
+                <?php echo '<p>Some description string</p>'; ?>
         </div>
     </div>
 </body>
-
 </html>
