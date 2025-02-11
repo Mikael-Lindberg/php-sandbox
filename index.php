e56543f990883035ccb41611be100ca63546d218
Mikael Lindberg <mikael.lindberg93@gmail.com>
2025-02-10 18:51:16 -0800

    initialized project

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+    echo 'Hello From PHP '
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>PHP Primer</title>
+</head>
+
+<body class="bg-gray-100">
+    <header class="bg-blue-500 text-white p-4">
+        <div class="container mx-auto">
+            <h1 class="text-3xl font-semibold">WOOOOOO</h1>
+        </div>
+    </header>
+    <div class="container mx-auto p-4 mt-4">
+        <div class="bg-white rounded-lg shadow-md p-6">
+            <h2 class="text-2xl font-semibold mb-4">PHP Primer</h2>
+            <p>Let's get going bois.</p>
+        </div>
+    </div>
+</body>
+
+</html>
